5dac7736862bc64fbc69762e5235b31503494e7b
Marc Jourdes <marcjourdes@gmail.com>
2018-05-28 10:38:54 +0200

    Add routes and notFoundComponent

--- client/coty/src/app/app.module.ts
+++ client/coty/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./navigation";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
