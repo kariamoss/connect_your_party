472ac7d3c43a2f213e1874542b88f69ad918cdd0
Lucas <lucas.matteo@etu.unice.fr>
2018-05-28 17:37:43 +0200

    updating interfaces PNSJ-27

--- server/back/src/main/java/ConnectYourParty/webInterface/photo/IPhotoModule.java
+++ server/back/src/main/java/ConnectYourParty/webInterface/photo/IPhotoModule.java
@@ -3,15 +3,16 @@ package ConnectYourParty.webInterface.photo;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 @Path("/photo")
 public interface IPhotoModule {
 
     @POST()
     @Path("addPhoto")
-    public String addPhoto();
+    public Response addPhoto();
 
     @GET()
     @Path("getPhotos")
-    public String getPhotos();
+    public Response getPhotos();
 }
--- server/back/src/main/java/ConnectYourParty/webInterface/photo/PhotoModule.java
+++ server/back/src/main/java/ConnectYourParty/webInterface/photo/PhotoModule.java
@@ -3,21 +3,31 @@ package ConnectYourParty.webInterface.photo;
 import ConnectYourParty.chooser.PhotoChooser;
 import ConnectYourParty.services.photo.IPhotoService;
 
+import javax.ws.rs.core.Response;
+import java.io.File;
+
 public class PhotoModule implements IPhotoModule {
 
     @Override
-    public String addPhoto() {
+    public Response addPhoto() {
         PhotoChooser photoChooser = new PhotoChooser();
         IPhotoService photoService = photoChooser.getService();
         //photoService.addPhoto(url);
-        return "salut";
+        return Response.ok().build();
     }
 
     @Override
-    public String getPhotos() {
-        PhotoChooser photoChooser = new PhotoChooser();
-        IPhotoService photoService = photoChooser.getService();
+    public Response getPhotos() {
+        //PhotoChooser photoChooser = new PhotoChooser();
+        //IPhotoService photoService = photoChooser.getService();
         //photoService.getPhotos(url);
-        return "Vla tes photos";
+        File photo = findPhoto();
+        Response.ResponseBuilder response = Response.ok((Object) photo);
+        response.header("Content-Disposition", "attachment;filename=photo.jpg");
+        return response.build();
+    }
+
+    public File findPhoto(){
+        return new File("/home/luquamateo/si4/connectyourparty/image.jpg");
     }
 }
