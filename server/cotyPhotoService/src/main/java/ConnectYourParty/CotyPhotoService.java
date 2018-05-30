95a66416ee9acea0813500dd3ee1af2a337c9842
jehan <jehanmilleret@gmail.com>
2018-05-30 11:29:53 +0200

    PNSJ-52 : Added delete photo functionality

--- server/cotyPhotoService/src/main/java/ConnectYourParty/CotyPhotoService.java
+++ server/cotyPhotoService/src/main/java/ConnectYourParty/CotyPhotoService.java
@@ -5,6 +5,7 @@ import ConnectYourParty.exceptions.photo.CannotDeletePhotoException;
 import ConnectYourParty.exceptions.photo.RetrievePhotoErrorException;
 import ConnectYourParty.services.photo.IPhotoService;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CotyPhotoService implements IPhotoService {
@@ -20,7 +21,7 @@ public class CotyPhotoService implements IPhotoService {
 
     @Override
     public void removePhoto(String path) throws CannotDeletePhotoException {
-
+        Photos.removePhoto(path);
     }
 
     @Override
@@ -30,6 +31,10 @@ public class CotyPhotoService implements IPhotoService {
 
     @Override
     public URL getServiceIcon() {
-        return null;
+        try {
+            return new URL("http://2.bp.blogspot.com/-V31y2Ef4Ad0/VZservQf70I/AAAAAAAAdu8/ErI--hbXwfE/s1600/OpenCamera1.png");
+        } catch (MalformedURLException e) {
+            return null;
+        }
     }
 }
