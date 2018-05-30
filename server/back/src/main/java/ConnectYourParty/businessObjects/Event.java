2e1a493c1f35169249c39b31e30574f3c34141ae
jehan <jehanmilleret@gmail.com>
2018-05-30 13:20:54 +0200

    PNSJ-44 : Mise en place d'une bdd mockée pour enregistrer l'emplacement des photos dans le système

---
+++ server/back/src/main/java/ConnectYourParty/businessObjects/Event.java
@@ -0,0 +1,44 @@
+package ConnectYourParty.businessObjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+    private int id;
+    private String title;
+    private int nbPeople;
+    private String location;
+    private List<Photo> photos;
+
+    public Event(int id, String title, int nbPeople, String location) {
+        this.id = id;
+        this.title = title;
+        this.nbPeople = nbPeople;
+        this.location = location;
+        this.photos = new ArrayList<>();
+    }
+
+    public void addPhoto(Photo photo){
+        this.photos.add(photo);
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNbPeople() {
+        return nbPeople;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
