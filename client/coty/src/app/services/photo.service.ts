688e853e9832360060af750e45aaea55a8adb9c3
Marc Jourdes <marcjourdes@gmail.com>
2018-06-01 11:10:42 +0200

    Set apiEndpoint

--- client/coty/src/app/services/photo.service.ts
+++ client/coty/src/app/services/photo.service.ts
@@ -1,20 +1,22 @@
-import {Injectable} from "@angular/core";
+import {Inject, Injectable} from "@angular/core";
 import {Subject} from "rxjs/internal/Subject";
 import {Photo} from "../../model/photo.model";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {APP_CONFIG, AppConfig} from "../app-config.module";
 
 @Injectable()
 export class PhotoService {
 
   photosSubject = new Subject<Photo[]>();
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              @Inject(APP_CONFIG) public config: AppConfig,) {
   }
 
   getPhotos(eventId: number): void {
     const headers = new HttpHeaders();
     headers.append("Content-Type", 'application/json');
-    const result = this.httpClient.get<Array<Photo>>('http://localhost:8080/back-1.0-SNAPSHOT/photo/getPhotoList', {headers: headers});
+    const result = this.httpClient.get<Array<Photo>>('http://'+ this.config.apiEndpoint +'/back-1.0-SNAPSHOT/photo/getPhotoList', {headers: headers});
     result.subscribe(data => {
         this.photosSubject.next(data.slice());
       },
