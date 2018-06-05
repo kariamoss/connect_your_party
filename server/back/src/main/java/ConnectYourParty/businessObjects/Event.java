0196b075b3722f8940bfb86fc391d759d8bc66c4
Lucas <lucas.matteo@etu.unice.fr>
2018-06-05 09:28:40 +0200

    removing phto list from event

--- server/back/src/main/java/ConnectYourParty/businessObjects/Event.java
+++ server/back/src/main/java/ConnectYourParty/businessObjects/Event.java
@@ -1,29 +1,16 @@
 package ConnectYourParty.businessObjects;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Event {
     private int id;
     private String title;
     private int nbPeople;
     private String location;
-    private List<Photo> photos;
 
     public Event(int id, String title, int nbPeople, String location) {
         this.id = id;
         this.title = title;
         this.nbPeople = nbPeople;
         this.location = location;
-        this.photos = new ArrayList<>();
-    }
-
-    public void addPhoto(Photo photo){
-        this.photos.add(photo);
-    }
-
-    public List<Photo> getPhotos() {
-        return photos;
     }
 
     public int getId() {
--- server/back/src/main/java/ConnectYourParty/database/DbMock.java
+++ server/back/src/main/java/ConnectYourParty/database/DbMock.java
@@ -15,6 +15,7 @@ public class DbMock {
     private List<Event> events = new ArrayList<>();
     private Event event = new Event(0, "La_grande_soirée_costumée", 65, "12, route de Virey 70700 Charcenne");
     private User user = new User("Milleret", "Jehan");
+    private List<Photo> photos = new ArrayList<>();
 
     public List<Event> getEvents() {
         return events;
@@ -29,23 +30,24 @@ public class DbMock {
     }
 
     public List<Photo> getPhotosFromEvent(){
-        return this.event.getPhotos();
+        return this.photos;
     }
 
     public void addPhoto(Event event, Photo photo) throws PhotoAlreadyExistException{
-        if(event.getPhotos().contains(photo)){
+        if(photos.contains(photo)){
             throw new PhotoAlreadyExistException();
         }
-        event.addPhoto(photo);
+        photos.add(photo);
         System.out.println("photo added");
     }
 
     public void clean(){
         event = new Event(0, "La_grande_soirée_costumée", 65, "12, route de Virey 70700 Charcenne");
+        photos = new ArrayList<>();
     }
 
     public String getServiceFromPath(String photoPath) throws NoSuchPhotoException{
-        for(Photo photo : event.getPhotos()){
+        for(Photo photo : photos){
             if(photo.getPhotoPath().equals(photoPath)){
                 return photo.getServiceHost();
             }
@@ -54,7 +56,7 @@ public class DbMock {
     }
 
     public Photo getPhotoFromPath(String path) throws NoSuchPhotoException{
-        for(Photo photo : event.getPhotos()){
+        for(Photo photo : photos){
             if(photo.getPhotoPath().equals(path)){
                 return photo;
             }
@@ -63,6 +65,6 @@ public class DbMock {
     }
 
     public void removePhotoFromEvent(Event event , Photo photo){
-        event.getPhotos().remove(photo);
+        photos.remove(photo);
     }
 }
