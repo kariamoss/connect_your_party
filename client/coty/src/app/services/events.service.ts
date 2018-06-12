be04267bb9d325bfaf901ab8eb452572b14685d8
Josué <jcr.clb@gmail.com>
2018-06-12 10:24:34 +0200

    updated front stuff

--- client/coty/src/app/services/events.service.ts
+++ client/coty/src/app/services/events.service.ts
@@ -10,7 +10,7 @@ export class EventService{
   private events: EventModel[] = [
     new EventModel(1,'Soirée barbecue','Moultes bons moments à partager entre amis sont prévus. Ce sera le feu ! 😂😂😂 Ramène ta chipo !!',23,'chez la daronne à Lucas','9 juin','bbq.jpg','party',this.colors[0]),
     new EventModel(2,'Piscine','Lucas veut aller à la piscine pour entretenir son corps alors on est sympa on l\'accompagne pour qu\'il soit pas seul',4,'à la piscine municipale','18 mars','piscine.jpg','party',this.colors[1]),
-    new EventModel(3,'Kermesse','on fait la fête pasqu\'on aura plus à voir la tête de lucas',1000,'à l\'ecole','18 juin','bowling.jpg','event',this.colors[2])
+    new EventModel(3,'Kermesse','on fait la fête pasqu\'on aura plus à voir la tête de lucas',1000,'à l\'ecole','18 juin','kermesse.jpg','event',this.colors[2])
   ];
   eventSubject = new Subject<EventModel[]>();
 
