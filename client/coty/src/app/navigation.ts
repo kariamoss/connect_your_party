ab24055e65ab7c07e1e24891943a02a815da1107
Josué <jcr.clb@gmail.com>
2018-06-13 11:32:30 +0200

    updated trucs

--- client/coty/src/app/navigation.ts
+++ client/coty/src/app/navigation.ts
@@ -8,22 +8,26 @@ import {MusicComponent} from "./music/music.component";
 import {DevelopersComponent} from "./developers/developers.component";
 import {AuthenticationProcessComponent} from "./authentication-process/authentication-process.component";
 import {PaymentComponent} from "./payment/payment.component";
+import {ProcessPaymentComponent} from "./process-payment/process-payment.component";
+import {CancelPaymentComponent} from "./cancel-payment/cancel-payment.component";
 
 export const appRoutes: Routes = [
 
 
-  {path: '', pathMatch: 'full',redirectTo: 'events'},
+  {path: '', pathMatch: 'full', redirectTo: 'events'},
   {path: 'events', component: EventViewComponent},
   {
     path: 'events/:id', component: EventComponent,
     children: [
-      {path: '', pathMatch: 'full',redirectTo: 'details'},
+      {path: '', pathMatch: 'full', redirectTo: 'details'},
       {path: 'details', component: EventDetailsComponent},
       {path: 'photo', component: PhotosComponent},
-      {path: 'music',component: MusicComponent},
-      {path: 'payment',component: PaymentComponent}
+      {path: 'music', component: MusicComponent},
+      {path: 'payment', component: PaymentComponent}
     ]
   },
+  {path: 'processPayment', component: ProcessPaymentComponent},
+  {path: 'cancelPayment', component: CancelPaymentComponent},
   {path: 'authentication', component: AuthenticationProcessComponent},
   {path: 'devs', component: DevelopersComponent},
   {path: 'not-found', component: NotFoundComponent},
