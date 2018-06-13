cca9fb66a5e3a56908ac11546fda5280cd782339
Marc Jourdes <marcjourdes@gmail.com>
2018-06-13 11:15:18 +0200

    PNSJ-64 Petit fail merge, impl of buildPayment for paypal

--- server/serviceInterface/src/main/java/connectYourParty/services/payment/IPaymentService.java
+++ server/serviceInterface/src/main/java/connectYourParty/services/payment/IPaymentService.java
@@ -6,10 +6,11 @@ import connectYourParty.objects.TokenService;
 import connectYourParty.services.IService;
 
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public interface IPaymentService extends IService {
-    URL buildPayment(String target, double amount, Optional<TokenService> token);
+    List<URL> buildPayment(String target, double amount, Optional<TokenService> token);
 
     void confirm(String payerId, Optional<TokenService> token);
 
